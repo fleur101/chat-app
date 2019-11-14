a6ace1c6bd4ad3a8a6cf855e99dbaaa351dd92b3
fleur101 <gaukhard96@gmail.com>
2019-11-14 23:15:27 +0200

    Init commit

---
+++ public/js/chat.js
@@ -0,0 +1,13 @@
+const socket = io()
+
+socket.on('message', (data) => {
+    console.log(data)
+})
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const message = e.target.elements.message.value
+
+    socket.emit('sendMessage', message)
+})
